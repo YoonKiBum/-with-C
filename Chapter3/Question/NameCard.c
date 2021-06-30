8c300ebe427d54c0c950a72e73f8fbe8a0800e05
KiBumYoon <62062154+YoonKiBum@users.noreply.github.com>
2021-06-30 10:25:39 +0900

    Create NameCard.c

---
+++ Chapter3/Question/NameCard.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "NameCard.h"
+
+NameCard* MakeNameCard(char* name, char* phone) {
+	NameCard* newCard = (NameCard*)malloc(sizeof(NameCard));
+	strcpy(newCard->name, name);
+	strcpy(newCard->phone, phone);
+	return newCard;
+}
+
+void ShowNameCardInfo(NameCard* pcard) {
+	printf("이름 : %s \n", pcard->name);
+	printf("전화번호 : %s \n", pcard->phone);
+}
+
+int NameCompare(NameCard* pcard, char* name) {
+	return strcmp(pcard->name, name);
+}
+
+void ChagePhoneNum(NameCard* pcard, char* phone) {
+	strcpy(pcard->phone, phone);
+}
